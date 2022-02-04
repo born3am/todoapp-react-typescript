0f18b44286e9a03c002c9ab19daf1a38621c00ed
born3am <psmaranhao@gmail.com>
2022-02-04 18:22:53 +0100

    material ui and sweetalert

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import "./App.css";
 import { v4 as uuid } from "uuid";
 import { TodoItemType } from "./interfaces";
 import TodoItem from "./TodoItem";
+import { Button, Container, TextField } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import DeleteIcon from "@mui/icons-material/Delete";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import { warningAlert, successAlert, deleteAlert } from "./alerts";
 
 function App() {
   const [todoList, setTodoList] = useState<TodoItemType[]>(
@@ -29,13 +34,27 @@ function App() {
       };
       setTodoList([...todoList, task]);
       setInputValue("");
+      successAlert();
     } else {
-      alert("please enter text in inputField");
+      warningAlert();
     }
   };
   //delete todo task
   const deleteTask = (id: string) => {
     setTodoList(todoList.filter((task) => task.id !== id));
+    deleteAlert();
+  };
+
+  //delete all Tasks
+  const deleteAllTasks = () => {
+    setTodoList([]);
+    deleteAlert();
+  };
+
+  //delete dones
+  const deleteTasksdone = () => {
+    setTodoList(todoList.filter((task) => !task.done));
+    deleteAlert();
   };
 
   // update task
@@ -49,37 +68,72 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>TODO APP</h1>
-        <input
-          className="App-taskInput"
-          type="text"
-          name="task"
-          onChange={handleChange}
-          value={inputValue}
-          placeholder="Add a todo"
-        />
-        <button className="App-Addbutton" onClick={addTask}>
-          Add Task
-        </button>
-      </header>
-      <main className="App-main">
-        <ul className="App-ul">
-          {todoList.map((task) => {
-            return (
-              <TodoItem
-                key={task.id}
-                task={task}
-                deleteTask={deleteTask}
-                statusTask={statusTask}
-              />
-            );
-          })}
-        </ul>
-      </main>
-      <footer className="App-footer">
-        <h5>Born3am - 2022</h5>
-      </footer>
+      <Container fixed>
+        <header className="App-header">
+          <h1>TODO APP</h1>
+          <div className="App-div-input">
+            <TextField
+              id="standard-basic"
+              label="Enter your task here!"
+              variant="standard"
+              type="text"
+              name="task"
+              onChange={handleChange}
+              value={inputValue}
+            />
+            <Button
+              size="small"
+              color="primary"
+              variant="contained"
+              startIcon={<AddCircleOutlineIcon />}
+              onClick={addTask}
+            >
+              Add Task
+            </Button>
+          </div>
+        </header>
+        <main className="App-main">
+          <ul className="App-ul">
+            {todoList.map((task) => {
+              return (
+                <TodoItem
+                  key={task.id}
+                  task={task}
+                  deleteTask={deleteTask}
+                  statusTask={statusTask}
+                />
+              );
+            })}
+
+            <div style={{ display: "flex", justifyContent: "right" }}>
+              <Button
+                sx={{ alignItem: "center" }}
+                size="small"
+                color="error"
+                endIcon={<DeleteIcon />}
+                onClick={() => deleteTasksdone()}
+              >
+                Delete Tasks Done
+              </Button>
+            </div>
+
+            <div style={{ display: "flex", justifyContent: "right" }}>
+              <Button
+                sx={{ alignItem: "center" }}
+                size="small"
+                color="error"
+                endIcon={<DeleteIcon />}
+                onClick={() => deleteAllTasks()}
+              >
+                Delete All
+              </Button>
+            </div>
+          </ul>
+        </main>
+        <footer className="App-footer">
+          <h5>Born3am - 2022</h5>
+        </footer>
+      </Container>
     </div>
   );
 }
